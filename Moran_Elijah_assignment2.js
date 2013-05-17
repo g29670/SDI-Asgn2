f12abda61cb89d9fed9948daadc8f7573f8a3c3f
g29670 <eliasM.2013@yahoo.com>
2013-05-16 21:44:38 -0400

    Update Moran_Elijah_assignment2.js
    2nd change to js file

--- Moran_Elijah_assignment2.js
+++ Moran_Elijah_assignment2.js
@@ -1,1 +1,18 @@
 //alert("JavaScript works!");
+//alert("JavaScript works!");
+//alert("JavaScript works!");
+// Author: Elijah Moran
+// Date: 005/16/2013
+// Project: Week 2 – Preparing for work.
+// Class: SDI 1305
+
+// variable declaration
+
+var theCurrentTime = 1500, 
+  numberOfMinutes = 10;
+
+var conversation1 = "is the coffee ready? ",
+	conversation2 = " he has to leave for work also in 10 minutes!";
+
+var itemsForWorkArray = ["uniform", "wallet", "cell-Phone", "identification"];
+var numberInArray = 2;
