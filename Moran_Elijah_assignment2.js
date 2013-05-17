8205e1388bce4c781b75b161f178eb92a53975ee
g29670 <eliasM.2013@yahoo.com>
2013-05-16 21:50:00 -0400

    Update Moran_Elijah_assignment2.js

--- Moran_Elijah_assignment2.js
+++ Moran_Elijah_assignment2.js
@@ -1,5 +1,5 @@
 //alert("JavaScript works!");
-
+//alert("JavaScript works!");
 // Author: Elijah Moran
 // Date: 005/16/2013
 // Project: Week 2 – Preparing for work.
@@ -8,10 +8,26 @@
 // variable declaration
 
 var theCurrentTime = 1500, 
-  numberOfMinutes = 10;
+	numberOfMinutes = 10;
 
 var conversation1 = "is the coffee ready? ",
 	conversation2 = " he has to leave for work also in 10 minutes!";
 
 var itemsForWorkArray = ["uniform", "wallet", "cell-Phone", "identification"];
 var numberInArray = 2;
+
+// First function asks if my alarm clock went off to wake me up
+// If it has, then it must be time to wake up
+// If not then I have a few more time to sleep
+var getOutOfBed = function (alarm)
+{
+	if (alarm === true)
+	{
+		console.log("Time to get out of bed!");
+	}
+	else
+	{
+		console.log("Well, my alarm did not go off, so I have more time " +
+		" to sleep.");
+	}
+}
